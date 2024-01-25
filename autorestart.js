0329cdda7c12cf1bcb986d967fa21faba4e30528
etnaks <157679351+etnaks@users.noreply.github.com>
2024-01-25 00:32:42 -0800

    Update autorestart.js

--- autorestart.js
+++ autorestart.js
@@ -21,16 +21,11 @@ var announceSent = false;
 
 // Rcon stuff
 var Rcon = require('rcon');
-var conn = new Rcon(serverip, serverport, rconpassword);
+var c = new Rcon(serverip, serverport, rconpassword);
 
 
-async function doLoop(c) {
-  c.send("");
+function doLoop() {
   var d = new Date(Date());
-  if (d.getMinutes() == 30 && !announceSent) { // Broadcast a message every hour at the 30 minute mark
-    c.send("Broadcast " + ("Server restarting at " + (restartHour + ":" + restartMinute+" "+ampm+" "+timezone)).replaceAll(" ", "-"));
-    announceSent = true; // Prevent command repeat
-  } else announceSent = false;
   if (d.getHours() == restartHour) {
     if (d.getMinutes() == restartMinute - 10 && !cmdSent) { // Broadcast message 10 minutes before restart
       c.send("Broadcast Server-restart-in-10-mins!");
@@ -41,21 +36,31 @@ async function doLoop(c) {
     } else if (d.getMinutes() == restartMinute && !cmdSent) {
       c.send("Broadcast Restarting!");
       c.send("Save");
-      setTimeout(function() { c.send("DoExit"); conn.disconnect(); conn.emit("end"); }, 3000) // Wait 3 secs before killing server to ensure it saved. Also close Rcon connection.
+      setTimeout(function() { c.send("DoExit"); c.disconnect(); c.emit("end"); }, 3000) // Wait 3 secs before killing server to ensure it saved. Also close Rcon connection.
       cmdSent = true; // Prevent command repeat
-    } else cmdSent = false;
+    } else { cmdSent = false; }
   }
+  if (d.getMinutes() == 30 && !announceSent) { // Broadcast a message every hour at the 30 minute mark
+    c.send("Broadcast " + ("Server restarting at " + (restartHour + ":" + restartMinute+" "+ampm+" "+timezone)).replaceAll(" ", "-"));
+    announceSent = true; // Prevent command repeat 
+  } else if (d.getMinutes() != 30) { announceSent = false; }
+  
 }
 
-conn.on('auth', function() {
+function keepalive() {
+  c.send("");
+}
+
+c.on('auth', function() {
   console.log("Authenticated");
-  var d = new Date(Date());
-  setInterval(function() { doLoop(conn) }, 10000); // Run function every 10 seconds
+  setInterval(function() { doLoop() }, 5000);
+  setInterval(function() { keepalive() }, 120000); // keep us connected to rcon by sending a blank command every two minutes
 }).on('error', function(err) {
   console.log("Error: " + err);
 }).on('end', function() {
   console.log("Connection closed. Waiting 45 seconds before reconnecting.");
-  setTimeout(function() { conn.connect(); }, 45000); // Reconnect after 45 seconds
+  setTimeout(function() { c.connect(); }, 45000); // Reconnect after 45 seconds
 });
 
-conn.connect();
+c.connect();
+
